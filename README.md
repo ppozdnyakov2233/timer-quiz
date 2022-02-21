fddddb8338b20b47e124bb0cbbdc33becdd90e30
ppozdnyakov <rpozdnyakov@hotmail.com>
2022-02-21 12:44:37 -0800

    finish

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # timer-quiz
+
+
+
+# Purpose 
+This quiz is to test your knowledge on Capital cities of the United States!
+
+
+# Built with
+* HTML
+* CSS
+* JAVASCRIPT
+
+
+# Website
+
+
+
+
+
+
+# Screenshot 
+
---
+++ assets/js/questions.js
@@ -0,0 +1,117 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What is the capitol of Massachusetts?",
+    answer: "Boston",
+    options: [
+      "Sacramento",
+      "Maryland",
+      "Boston",
+      "Chicago"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the capitol of California?",
+    answer: "Sacramento",
+    options: [
+      "Boston",
+      "Phoenix",
+      "Atlanta",
+      "Sacramento"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the capitol of Florida?",
+    answer: "Tallahassee",
+    options: [
+      "Boise",
+      "Springfield",
+      "Tallahassee",
+      "Topeka"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the capitol of Maine?",
+    answer: "Augusta",
+    options: [
+      "Boston",
+      "Saint Paul",
+      "Jackson",
+      "Augusta"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the capitol of Ohio?",
+    answer: "Columbus",
+    options: [
+      "Columbus",
+      "Salem",
+      "Pierre",
+      "Austin"
+    ]
+  },
+     {
+     numb: 6,
+     question: "What is capitol of Pennsylvania?",
+     answer: "Harrisburg",
+     options: [
+       "Bismarck",
+       "Concord",
+       "Harrisburg",
+       "Helena"
+     ]
+ },
+
+ {
+  numb: 7,
+  question: "What is the capitol of Nebraska?",
+  answer: "Lincoln",
+  options: [
+    "Lincoln",
+    "Boston",
+    "Lansing",
+    "Frankfort"
+  ]
+},
+
+{
+  numb: 8,
+  question: "What is the capitol of Kansas ?",
+  answer: "Topeka",
+  options: [
+    "Boise",
+    "Topeka",
+    "Helena",
+    "Raleigh"
+  ]
+},
+
+{
+  numb: 9,
+  question: "What is the capitol of New Jersey?",
+  answer: "Trenton",
+  options: [
+    "Concord",
+    "Kursk",
+    "Albany",
+    "Trenton"
+  ]
+},
+
+{
+  numb: 10, 
+  question: "What is the capitol of Wyoming?",
+  answer: "Cheyenne",
+  options: [
+    "Richmond",
+    "Pierre",
+    "Cheyenne",
+    "Moscow"
+  ]
+}
+
+];
